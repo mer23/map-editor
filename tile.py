698ce371a728b7007b601ced17d2cf9f4dea98b9
Ignacio M. Tirabasso <ignacio.tirabasso@gmail.com>
2014-06-08 13:28:48 -0300

    Tampoco debería saber a que pos pertenece y hay mas de una surface por tile

--- tile.py
+++ tile.py
@@ -3,25 +3,18 @@
 class Tile(object):
 	"""Represents a single tile in the map. 
 
-	Attributes: map, x, y, surface, entity."""
+	Attributes: surface, entities ."""
 
-	def __init__(self, x= 0, y= 0, surface= None, entity= None, blocked= False, jump_over= False):
-
-		self.x= x
-		self.y= y
-		self.surface= surface
-		self.entity= entity
-		self.blocked= blocked
-		self.jump_over= jump_over
+	def __init__(self, layers, entities = [], blocked = False, jump_over = False):
+		self.layers = layers
+		self.entities = entities
+		self.blocked = blocked
+		self.jump_over = jump_over
 
 	def __str__(self):
 
-		return ("position: " + str(self.pos()) +
-				" || surface: " + self.surface +
+		return (" || layers: " + self.layers +
 				" || sprite: " + self.entity + "\n"
 				"blocked: " + self.blocked +
 				" || jump_over: " + self.jump_over + "\n")
 
-
-	def pos(self):
-		return (self.map, self.x, self.y)
